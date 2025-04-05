8bb90a1e7aef17ea63d325a139a2fb596614f4a3
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2025-04-05 16:37:39 +0200

    feat: users module : first routing and services - getAllUsers & getOneUser

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
 
+/*
+GET /users
+GET /users/:id
+POST /users
+PATCH /users/:id
+DELETE /users/:id
+*/
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    // Tous les utilisateurs
+    @Get() // GET /users
+    getAllUsers(): string {
+      return this.usersService.getAllUsers();
+    }
+    // Un utilisateur par son id
+    @Get(':id') // GET /users/:id
+    getOneUser(@Param('id')id:string): string {
+      return this.usersService.getOneUser(id);
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    getAllUsers():string{
+        return "<h1>Tous les utilisateurs</h1>";
+    };
+    getOneUser(id: string):string{
+        return "<h1>Utilisateur n° "+id+"</h1>";
+    }
+}
