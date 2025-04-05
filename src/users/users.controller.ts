18c1ea06dfd54b40aedb0219f8ea9607bacebfa0
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2025-04-05 18:25:14 +0200

    feat: users : POST new user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Param, Query } from '@nestjs/common';
+import {Body, Controller, Get, Post, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 /*
@@ -14,12 +14,18 @@ export class UsersController {
 
     // Tous les utilisateurs
     @Get() // GET /users OU GET /users?role=unRoleDefini
-    getAllUsers(@Query()query:{role?:string; city?:string}): string {
-      return this.usersService.getAllUsers(query.role, query.city);
+    getAllUsers(): string {
+      return this.usersService.getAllUsers();
     }
     // Un utilisateur par son id
     @Get(':id') // GET /users/:id
     getOneUser(@Param('id')id:string): string {
       return this.usersService.getOneUser(id);
+    }
+
+    // Ajouter un utilisateur
+    @Post() // POST /users
+    createUser(@Body()NewUserData:{}): {}{
+        return this.usersService.createUser(NewUserData);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,22 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-    getAllUsers(role?:string, city?:string):string{
-        
-        if(role || city){
-            let resultString = "<h1>Les utilisateurs :</h1>";
-        if(role){
-            resultString += "<p>Avec le rôle: "+role+"</p>";
-        }
-        if(city){
-            resultString += "<p>De la ville: "+city+"</p>";
-        }
-            return resultString;
-        }else{
-            return "<h1>Tous les utilisateurs</h1>";
-        }
+    getAllUsers():string{
+        return "<h1>Tous les utilisateurs</h1>";
     };
     getOneUser(id: string):string{
         return "<h1>Utilisateur n° "+id+"</h1>";
+    }
+    createUser(newUser:{}):{}{
+        return newUser;
     }
 }
