44304df746ae422c86a3971e862929fb5ab7037f
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2025-04-16 18:52:54 +0200

    fix: Owner and Tenant's primary key is foreign key userId

--- src/owners/owner.dto.ts
+++ src/owners/owner.dto.ts
@@ -1,10 +1,10 @@
 import { AddressDTO } from 'src/addresses/address.dto';
+import { LocationDTO } from 'src/locations/location.dto';
 import { UserDTO } from 'src/users/user.dto';
 
 export class OwnerDTO extends UserDTO{
 
   address?:AddressDTO;
 
-  // Classe à implémenter
-  //locations:LocationDTO[];
+  locations:LocationDTO[];
 }
