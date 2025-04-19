2c1fd51e2a5341f62c7c5574ae161e4494badb62
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2025-04-19 15:41:03 +0200

    feat: location.service getAllLocationsByOwner

--- src/locations/location-summary.dto.ts
+++ src/locations/location-summary.dto.ts
@@ -1,8 +1,6 @@
-import { Exclude, Type } from 'class-transformer';
-import { IsBoolean, IsDate, IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsBoolean, IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
 import { AddressDTO } from 'src/addresses/address.dto';
-import { OwnerDTO } from 'src/owners/owner.dto';
-import { TenantDTO } from 'src/tenants/tenant.dto';
 
 export class LocationSummaryDTO{
     
@@ -22,10 +20,7 @@ export class LocationSummaryDTO{
     @IsOptional()
     @Type(()=>AddressDTO)
     address?:AddressDTO;
-    
+
     @IsBoolean()
     rented:boolean = false;
-
-    @Type(()=>OwnerDTO)
-    owner:OwnerDTO;
 }
--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -1,48 +1,40 @@
 import { Injectable } from '@nestjs/common';
-import { OwnershipDTO } from 'src/ownerships/ownership.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 // Les DTO
-import { LocationDetailDTO } from '../locations/location-detail.dto';
 import { LocationSummaryDTO } from '../locations/location-summary.dto';
+import { LocationDetailDTO } from './location-detail.dto';
+import { OwnershipDTO } from 'src/ownerships/ownership.dto';
 
 @Injectable()
 export class LocationsService {
 
     constructor(private prisma:PrismaService){}
 
-    async getAllLocationsBelongingToOwner(ownerId:number):Promise<{}[]|[]>{
+    async getAllLocationsByOwner(ownerId: number): Promise<LocationSummaryDTO[]> {
 
-        let locations:{}[] = [];
-         try{
-             // 1 - Dans la table de liaison ownerships : trouver toutes les relations qui concerne l'id du propriétaire
-             const ownerships:OwnershipDTO[] = await this.prisma.ownership.findMany({
-                 where:{ownerId},
-                 // 2 - Pour chaque relation trouvée : inclure les données du logement
-                 include:{location:true}
-             });
-             // 3 - Si le propriétaire est bien lié à un logement dans cette ligne de la table
-             ownerships.forEach((ownership)=> {
-                 if (ownership.location) {
-                     locations.push({
-                       id: ownership.location.id,
-                       name:ownership.location.name,
-                       type:ownership.location.type,
-                       rooms:ownership.location.rooms,
-                       EPD:ownership.location.EPD,
-                       surface:ownership.location.surface,
-                       furnished:ownership.location.furnished,
-                       rented:ownership.location.rented,
-                       rent:ownership.location.rent,
-                       acquiredAt: ownership.acquiredAt,
-                       createdAt:ownership.location.createdAt,
-                       updatedAt:ownership.location.updatedAt,
-                     });
-                 }
-             })
-         }catch(error){
-             console.error("Aucun logement enregistré", error); 
-         }
-         return locations;
-     }
+        const ownerships:OwnershipDTO[] = await this.prisma.ownership.findMany({
+          where: { ownerId },
+          // Pour chaque Ownership, inclure la location
+            include: { 
+                // Pour chaque location, inclure l'adresse
+                location:{
+                    include: { address: true }
+                } 
+            },
+        });
+
+      // Transformer les OwnershipDTO[] en LocationSummaryDTO[]
+        return ownerships
+        // Ne garder que les Ownerships ayant une Location associé
+          .filter(ownership => ownership.location !== null)
+          // Mapper chaque Ownership vers un LocationSummaryDTO : À partir du tableau des Ownerwships, retourner un nouveau tableau en transformant chaque élément en LocationDTO
+          .map(ownership => ({
+            id: ownership.location.id,
+            name: ownership.location.name,
+            type: ownership.location.type,
+            address: ownership.location.address,
+            rented: ownership.location.rented,
+          }));
+    }
 }
