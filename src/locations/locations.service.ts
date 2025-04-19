0750932419e778cfdd1e14eb8eb67504ccde1137
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2025-04-19 15:58:37 +0200

    feat: locations.service getAllLocationsByOwner : handle errors

--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 // Les DTO
@@ -13,28 +13,35 @@ export class LocationsService {
 
     async getAllLocationsByOwner(ownerId: number): Promise<LocationSummaryDTO[]> {
 
-        const ownerships:OwnershipDTO[] = await this.prisma.ownership.findMany({
-          where: { ownerId },
-          // Pour chaque Ownership, inclure la location
-            include: { 
-                // Pour chaque location, inclure l'adresse
-                location:{
-                    include: { address: true }
-                } 
-            },
-        });
+        try{
+            const ownerships:OwnershipDTO[] = await this.prisma.ownership.findMany({
+                where: { ownerId },
+                // Pour chaque Ownership, inclure la location
+                  include: { 
+                      // Pour chaque location, inclure l'adresse
+                      location:{
+                          include: { address: true }
+                      } 
+                  },
+            });
 
-      // Transformer les OwnershipDTO[] en LocationSummaryDTO[]
-        return ownerships
-        // Ne garder que les Ownerships ayant une Location associé
-          .filter(ownership => ownership.location !== null)
-          // Mapper chaque Ownership vers un LocationSummaryDTO : À partir du tableau des Ownerwships, retourner un nouveau tableau en transformant chaque élément en LocationDTO
-          .map(ownership => ({
-            id: ownership.location.id,
-            name: ownership.location.name,
-            type: ownership.location.type,
-            address: ownership.location.address,
-            rented: ownership.location.rented,
-          }));
+            // Transformer les OwnershipDTO[] en LocationSummaryDTO[]
+            return ownerships
+            // Ne garder que les Ownerships ayant une Location associé
+            .filter(ownership => ownership.location !== null)
+            // Mapper chaque Ownership vers un LocationSummaryDTO : À partir du tableau des Ownerwships, retourner un nouveau tableau en transformant chaque élément en LocationDTO
+            .map(ownership => ({
+                id: ownership.location.id,
+                name: ownership.location.name,
+                type: ownership.location.type,
+                address: ownership.location.address,
+                rented: ownership.location.rented,
+            }));
+        }catch(error){
+            // Intercepter l'erreur
+            console.error(`Erreur lors de la récupération des logements du propriétaire ${ownerId}`, error);
+            // Renvoyer une erreur nestjs pour le contrôleur
+            throw new InternalServerErrorException('Impossible de récupérer les logements');
+        }
     }
 }
